db9428640b66567bfb62c75da500275fdeeab71e
alokdubey01 <alok73327@gmail.com>
2025-03-15 15:26:43 +0530

    server api added

--- backend/api/index.js
+++ backend/api/index.js
@@ -4,17 +4,18 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const userRoutes = require("./routes/user");
-const storyRoutes = require("./routes/story");
-const { verifyToken } = require("./utils/token");
+const userRoutes = require("../routes/user");
+const storyRoutes = require("../routes/story");
+const { verifyToken } = require("../utils/token");
 
 const app = express();
-const port = 5000;
+// const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-const mongoURI = "mongodb://127.0.0.1:27017/veebo";
+// const mongoURI = "mongodb://127.0.0.1:27017/veebo";
+const mongoURI = process.env.MONGO_URI;
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
 connection.once("open", () => {
@@ -36,6 +37,8 @@ app.get("/admin", verifyToken, (req, res) => {
   });
   
 
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is running on port: ${port}`);
+// });
+
+module.exports = app;
